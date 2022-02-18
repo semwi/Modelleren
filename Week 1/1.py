6c782a320e9307d02be16b6e5893f34da8fa2ac8
semwi <20000529@student.hhs.nl>
2022-02-18 20:33:58 +0100

    Create 1.py

---
+++ Week 1/1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 18 20:27:08 2022
+
+@author: Sem
+"""
+
+import numpy as np
+x = np.linspace(0, 25, 26)
+lijst_even = (x[x%2==0])
+
+lijst_tot_drie = np.linspace(1,3,10)
